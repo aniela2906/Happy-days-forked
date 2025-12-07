25f5f484e4997b556b2e24b340f6d018b1039dbb
pecxpecx <eava@itu.dk>
2025-12-07 20:58:00 +0100

    Deleted hello.py, redundant

--- go/python-files/hello.py
+++
@@ -1,7 +0,0 @@
-# Write to a file in a new directory called "output"
-import os
-
-os.makedirs("output", exist_ok=True)
-
-with open("output/hello-from-python.txt", "w") as f:
-    f.write("Hello from Python in Dagger!\n")
